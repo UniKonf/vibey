eaf2d8fc8931b67cb30c79fb9a5173bbe23e508f
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-11-11 19:10:46 +0530

    implement event saving to algolia client

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,19 @@
 import { NextPage } from 'next';
 import { Events, Heading, SearchBar } from '../components';
+import { Results } from '../components/Search/Results';
+import { index } from '../lib/AlgoliaClent';
 import { useGetEvents } from '../lib/hooks/useGetEvents';
 
 const Home: NextPage = () => {
   const { events, loading, error } = useGetEvents();
+  if (events != null)
+    void index.saveObjects(
+      events.map((event) => ({ objectID: event.id, ...event }))
+    );
   return (
     <>
       <SearchBar />
+      <Results />
       <div className="flex flex-col gap-6">
         <Heading
           title="Upcoming Events"
