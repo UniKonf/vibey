4628649cd57f750f16b8c5adbff46f76e382bcdd
0papi <evanskwofie67@gmail.com>
2022-12-06 05:16:39 +0000

    uncomment supabase config

--- src/lib/db/supabaseClient.ts
+++ src/lib/db/supabaseClient.ts
@@ -1,13 +1,13 @@
-// import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js';
 
-// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-// const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-// if (supabaseUrl == null || supabaseAnonKey == null) {
-//   throw new Error(
-//     'Please define the NEXT_PUBLIC_SUPABASE_URL and NEXT_PUBLIC_SUPABASE_ANON_KEY environment variables inside .env.local'
-//   );
-// }
+if (supabaseUrl == null || supabaseAnonKey == null) {
+  throw new Error(
+    'Please define the NEXT_PUBLIC_SUPABASE_URL and NEXT_PUBLIC_SUPABASE_ANON_KEY environment variables inside .env.local'
+  );
+}
 
-// const test = createClient(supabaseUrl, supabaseAnonKey);
-// export const supabase = test;
+const test = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = test;
