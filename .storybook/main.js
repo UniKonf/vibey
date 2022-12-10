1f6d28c8fd61cef5e3ac35c3dbda140330edb6c9
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-12-10 12:21:21 +0530

    chore(storybook): add path alias to story book

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,27 +1,37 @@
-module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    'storybook-css-modules',
-    '@storybook/addon-interactions',
-    {
-      name: '@storybook/addon-postcss',
-      options: {
-        cssLoaderOptions: {
-          // When you have splitted your css over multiple files
-          // and use @import('./other-styles.css')
-          importLoaders: 1,
-        },
-        postcssLoaderOptions: {
-          // When using postCSS 8
-          implementation: require('postcss'),
-        },
+import { resolve } from 'path';
+
+export const stories = [
+  '../src/**/*.stories.mdx',
+  '../src/**/*.stories.@(js|jsx|ts|tsx)',
+];
+export const addons = [
+  '@storybook/addon-links',
+  '@storybook/addon-essentials',
+  'storybook-css-modules',
+  '@storybook/addon-interactions',
+  {
+    name: '@storybook/addon-postcss',
+    options: {
+      cssLoaderOptions: {
+        // When you have splitted your css over multiple files
+        // and use @import('./other-styles.css')
+        importLoaders: 1,
+      },
+      postcssLoaderOptions: {
+        // When using postCSS 8
+        implementation: require('postcss'),
       },
     },
-  ],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-webpack5',
   },
+];
+export const framework = '@storybook/react';
+export async function webpackFinal(config) {
+  config.resolve.alias = {
+    ...config.resolve.alias,
+    '@': resolve(__dirname, '../src/'),
+  };
+  return config;
+}
+export const core = {
+  builder: '@storybook/builder-webpack5',
 };
