87fe06afb0c00307e27bb19beb8266deb400127c
Haris <harisshah385@gmail.com>
2022-12-14 22:54:07 +0500

    Remove main container bottom border radius

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
   return (
     <>
       <Hero />
-      <div className="relative z-10 rounded-3xl border-2 border-primary bg-base-100 supports-[backdrop-filter:blur(0px)]:bg-base-100/50 supports-[backdrop-filter:blur(0px)]:backdrop-blur-lg">
+      <div className="relative z-10 rounded-t-3xl border-2 border-primary bg-base-100 supports-[backdrop-filter:blur(0px)]:bg-base-100/50 supports-[backdrop-filter:blur(0px)]:backdrop-blur-lg">
         <div className="container mx-auto flex max-w-6xl flex-col gap-14 py-5 px-2 pb-2">
           <SearchBar />
           <div className="flex flex-col gap-6">
