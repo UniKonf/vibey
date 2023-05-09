68002d3e488d7be5bcf59e25ee069802cb6467cc
Tanishqa Mitra <tanishqamitra79@gmail.com>
2023-05-09 23:03:39 +0530

    feat: added two events

--- src/constant/events.ts
+++ src/constant/events.ts
@@ -3,47 +3,52 @@ import { EventInterface } from '@/lib/types';
 const events: EventInterface[] = [
   {
     id: 1,
-    title: 'API Day @BITS Pilani',
+    title: 'JSConf India 2023',
     description:
-      'We are proud to celebrate the first edition of "API Day" @ BITS Pilani, Pilani Campus on 15 April 2023.  This is a one-day event, which aims to educate and inspire students about the importance of APIs and how they have revolutionized the world of technology.',
-    startTime: '15 April 2023 9:00' as unknown as Date,
-    endTime: '15 April 2023 17:00' as unknown as Date,
-    link: 'https://www.bits-postman-lab.in/events',
+      'Join the Indian edition of the most prestigious JS conference from the International JavaScript community.An International 1-day conference.',
+    startTime: '2 June 2023 9:00' as unknown as Date,
+    endTime: '2 June 2023 19:00' as unknown as Date,
+    link: 'https://jsconf.in/',
     socials: [
       {
         name: 'linkedin',
-        link: 'https://www.linkedin.com/company/bits-pilani-postman-lab/',
-      },
-    ],
-    themes: ['APIs', 'Postman', 'Open Source'],
-  },
-  {
-    id: 1,
-    title: 'KubeCon + CloudNativeCon Europe 2023',
-    description:
-      'KubeCon + CloudNativeCon Europe 2023 has SOLD OUT for in-person tickets and the waitlist has now been closed. For those on the waitlist, notifications will be sent via email by 7 April, 2023 (PDT).',
-    startTime: '18 April 2023 8:00' as unknown as Date,
-    endTime: '21 April 2023 18:00' as unknown as Date,
-    link: 'https://www.bits-postman-lab.in/events',
-    socials: [
-      {
-        name: 'linkedin',
-        link: 'https://www.linkedin.com/company/cloud-native-computing-foundation/',
+        link: 'https://www.linkedin.com/company/jsconf-india/',
       },
       {
         name: 'twitter',
-        link: 'https://twitter.com/CloudNativeFdn',
-      },
-      {
-        name: 'youtube',
-        link: 'https://www.youtube.com/c/cloudnativefdn',
-      },
-      {
-        name: 'facebook',
-        link: 'https://www.facebook.com/CloudNativeComputingFoundation/',
+        link: 'https://twitter.com/jsconf_india',
       },
     ],
-    themes: ['APIs', 'Postman', 'Open Source'],
+    themes: ['javascript', 'networking', 'workshops'],
+  },
+  {
+    id: 1,
+    title: 'KubeHuddle 2023',
+    description:
+      'KubeHuddle  conference where Developers, Platform Engineers, DevOps, SRE, Cloud Enthusiasts, Technical and Business Strategists come together to learn from each other, collaborate etc.',
+    startTime: '17 May 2023 6:30' as unknown as Date,
+    endTime: '18 May 2023 5:45 PM' as unknown as Date,
+    link: 'https://kubehuddle.com/2023/toronto/',
+    socials: [
+      {
+        name: 'linkedin',
+        link: 'https://www.linkedin.com/company/kubehuddle/?originalSubdomain=uk',
+      },
+      {
+        name: 'twitter',
+        link: 'https://twitter.com/KubeHuddle',
+      },
+      {
+        name: 'discord',
+        link: 'https://discord.com/invite/ErVgHCN',
+      },
+    ],
+    themes: [
+      'Cloud technologies',
+      'Cloud Native and Kubernetes',
+      'Technical Strategy',
+      'App Development challenges',
+    ],
   },
 ];
 
