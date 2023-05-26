5a182c8b944acadec83604862393b3daec4ca44a
Nayan Dey <133354699+nayan-dey@users.noreply.github.com>
2023-05-27 00:00:25 +0530

    made the 404.txt responsive (#191)

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,15 +5,15 @@ import React from 'react';
 const Custom404 = () => {
   return (
     <>
-      <div className="flex min-h-screen flex-col items-center justify-center">
-        <h1 className="mb-4 text-8xl font-bold">404 - Page Not Found</h1>
-        <p className="text-2xl">
-          {' '}
+      <div className="flex min-h-screen flex-col items-center justify-center text-center">
+        <h1 className="mb-4 text-3xl font-bold md:text-8xl">
+          404 - Page Not Found
+        </h1>
+        <p className="text-base md:text-2xl">
           Oops! This page is under construction, but you can help make it
           faster!
         </p>
-        &nbsp;
-        <span className="block text-xl">
+        <span className="block text-sm md:text-xl">
           Contribute to our open-source project and help improve the experience
           for everyone.
         </span>
