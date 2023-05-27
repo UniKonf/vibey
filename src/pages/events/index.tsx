6ab3ea853259b82f3dd71797006880580214546d
Suzy <sulagnaghosh52@gmail.com>
2023-05-27 23:23:20 +0530

    feat:add cities

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -1,9 +1,11 @@
-import { sortEventsByDate } from '@/lib/helper';
+import { sortEventsByFilter } from '@/lib/helper';
 import { EventType } from '@/lib/types';
 
 import Event from '@/components/upcoming/Event';
 
 import { NextPage } from 'next';
+import { useState } from 'react';
+import { BsFillFunnelFill } from 'react-icons/bs';
 
 const events: EventType[] = [
   {
@@ -41,7 +43,27 @@ const events: EventType[] = [
   },
 ];
 
+const cities = [
+  'Kolkata',
+  'New Delhi',
+  'Goa',
+  'Mumbai',
+  'Bangalore',
+  'Hyderabad',
+  'Chennai',
+];
 const EventPage: NextPage = () => {
+  const [chosenCity, setChosenCity] = useState<string>('');
+  const [showFilter, setShowFilter] = useState<boolean>(false);
+  const handleCityEvents = (city: string): void => {
+    setChosenCity(city);
+    setShowFilter(false);
+  };
+
+  const handleShowFilter = () => {
+    setShowFilter(!showFilter);
+  };
+
   return (
     <div className="relative z-10 rounded-3xl">
       <div className="layout mx-auto flex max-w-6xl flex-col gap-14 py-10 px-2">
@@ -50,11 +72,45 @@ const EventPage: NextPage = () => {
             Upcoming Events
           </span>
         </div>
+        <div>
+          <button
+            className="bg-black-500 flex cursor-pointer items-stretch rounded-xl border border-gray-400 bg-gray-100 py-2 px-4 font-semibold text-gray-800 shadow "
+            onClick={() => handleShowFilter()}
+          >
+            {' '}
+            <h3>Filter</h3>{' '}
+            <span className="pt-3 pl-4">
+              <BsFillFunnelFill />
+            </span>
+          </button>
 
-        <div className="events grid grid-cols-auto-sm gap-7">
-          {sortEventsByDate(events).map((event) => (
-            <Event key={event.id} {...event} />
-          ))}
+          <div className="events mt-5 grid cursor-pointer grid-cols-auto-sm gap-7">
+            {showFilter &&
+              cities.map((city, index) => (
+                <div
+                  key={index}
+                  onClick={() => handleCityEvents(city)}
+                  className="cursor:pointer rounded-xl  bg-base-100/70 text-center sm:pb-10 sm:pt-7 "
+                >
+                  <h3 className="text-sm ">{city}</h3>
+                </div>
+              ))}
+          </div>
+        </div>
+        <div>
+          {sortEventsByFilter(events, chosenCity).length > 0 ? (
+            <div className="events grid grid-cols-auto-sm gap-7">
+              {sortEventsByFilter(events, chosenCity).map((event) => (
+                <Event key={event.id} {...event} />
+              ))}
+            </div>
+          ) : (
+            <div className="rounded-3xl bg-base-100/70 px-6 py-5 text-center text-xl text-transparent md:pb-20 md:pt-14 ">
+              <span className="bg-gradient-to-bl from-[rgb(178,15,255)] to-[#ff5100] bg-clip-text ">
+                No Upcoming Events in {chosenCity}
+              </span>
+            </div>
+          )}
         </div>
       </div>
     </div>
