3dd4224c1357ff76ef218ef1902422eea4ed13aa
isha988 <ishakapoor.988@gmail.com>
2023-05-31 20:00:07 +0530

    add new event section deleted

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,7 @@
-import UnstyledLink from '@/components/links/UnstyledLink';
-
 import events from '@/constant/events';
 
 import { Events, Heading, Hero, SearchButton } from '../components';
 import { NextPage } from 'next';
-import Image from 'next/image';
 
 const Home: NextPage = () => {
   return (
@@ -26,65 +23,6 @@ const Home: NextPage = () => {
               href="/upcoming"
             />
             <Events events={events} />
-          </section>
-
-          {/* Add new event section */}
-          <section
-            className="relative w-full py-[80px] sm:py-[120px]"
-            id="add-event"
-          >
-            {/* background */}
-            <div className="bg opacity-40">
-              <Image
-                src="/images/homepage/addNewEvent.png"
-                alt="hero-image"
-                layout="fill"
-                className="opavity-40 object-cover lg:object-fill"
-              />
-            </div>
-            {/* section body */}
-            <div className="layout relative flex flex-col gap-14 py-10">
-              <div className="flex flex-col gap-5">
-                {/* heading */}
-                <Heading title="Add your Event" />
-                <div className="max-w-max text-base sm:text-2xl">
-                  {/* steps */}
-                  <ul className="list-inside list-disc text-color-pink">
-                    <li>
-                      <span className="text-foreground">
-                        Open a new issue{' '}
-                        <UnstyledLink
-                          href="https://github.com/UniKonf/vibey"
-                          className="text-color-pink hover:underline"
-                        >
-                          UniKonf/vibey
-                        </UnstyledLink>
-                      </span>
-                    </li>
-                    <li>
-                      {' '}
-                      <span className="text-foreground">
-                        Add Event details in the issue.{' '}
-                      </span>{' '}
-                    </li>
-                    <li>
-                      {' '}
-                      <span className="text-foreground">
-                        {' '}
-                        Submit issue.{' '}
-                      </span>{' '}
-                    </li>
-                    <li>
-                      {' '}
-                      <span className="text-foreground">
-                        {' '}
-                        That’s it. Just that.{' '}
-                      </span>{' '}
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
           </section>
         </div>
       </div>
