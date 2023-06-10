57f20b19cad986daba0ecd4d1ca53bad584c1e24
Vishal K <116670999+Vishalk91-4@users.noreply.github.com>
2023-06-10 16:30:16 +0530

    Updated about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -37,8 +37,8 @@ const about = () => {
           </span>
           <h3 className="hover font-light">All that's happening around in the Tech World</h3>   
       </div>
-      <div className="mx-20 justify-center flex overflow-hidden">
-        <div className="card justify-center text-center px-0 pt-0 mr-9 w-min">
+      <div className="mx-20 justify-center items-center overflow-hidden grid grid-cols-1 gap-3 sm:text-left sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
+        <div className="card justify-center text-center px-0 pt-0 mx-auto w-min">
           <Image width={200} height={200} src= "/static/event.jpg" alt="logo" className=" rounded-lg w-full h-56 dark:hidden text-center mx-auto overflow-hidden "/> 
           <Image width={200} height={200} src="/static/event.jpg" alt="logo" className="rounded-lg w-full h-56 hidden dark:block text-center overflow-hidden"/>     
           <h2 className="mt-10">Events</h2>
@@ -49,7 +49,7 @@ const about = () => {
           >Know More
           </Button></Link>
         </div>       
-        <div className="card justify-center text-center px-0 pt-0 mr-9 w-min">
+        <div className="card justify-center text-center px-0 pt-0 mx-auto w-min">
           <Image width={200} height={200} src= "/static/hackathon.jpg" alt="logo" className="rounded-lg w-full h-56 dark:hidden text-center mx-auto overflow-hidden"/> 
           <Image width={200} height={200} src="/static/hackathon.jpg" alt="logo" className="rounded-lg w-full h-56 hidden dark:block text-center overflow-hidden"/>     
           <h2 className="mt-10">Conference</h2>
@@ -60,7 +60,7 @@ const about = () => {
           >Know More
           </Button></Link>
         </div>   
-        <div className="card justify-center text-center px-0 pt-0 mr-9 w-min">
+        <div className="card justify-center text-center px-0 pt-0 mx-auto w-min">
           <Image width={200} height={200} src= "/static/cfp.jpg" alt="logo" className="rounded-lg h-56 w-full dark:hidden text-center mx-auto overflow-hidden"/> 
           <Image width={200} height={200} src="/static/cfp.jpg" alt="logo" className="rounded-lg h-56 w-full hidden dark:block text-center overflow-hidden"/>     
           <h2 className="mt-10">CFPs</h2>
@@ -72,31 +72,31 @@ const about = () => {
           </Button></Link>
         </div>   
       </div>
-      <div className="justify-content pl-6 mx-auto w-5/6 gap-10 flex mb-14">
-        <div className="text-center pt-40 mt-5 basis-1/2 md:mt-0 font-bold sm:text-lg md:text-xl lg:text-2xl xl:text-4xl">
+      <div className="justify-content pl-6 mx-auto w-11/12 gap-10 md:flex ">  
+        <div className="text-center pt-40 mt-5 basis-1/2  font-bold sm:text-lg md:text-xl lg:text-2xl xl:text-4xl">
           <span className="pt-40 text-transparent bg-gradient-to-bl from-[rgb(178,15,255)] to-[#ff5100] bg-clip-text">
-            Our Vision behind creating Vibey 
+          Our Vision behind creating Vibey
           </span>          
           <p className="flex mt-4"></p>
-          <p className=" flex text-sm font-light sm:text-sm md:text-lg lg:text-xl xl:text-2xl pl-15">
-          Lacus viverra vitae congue eu consequat ac. Diam quis enim lobortis scelerisque 
-          fermentum dui faucibus in ornare. Aliquet eget sit amet tellus cras adipiscing. 
-          Lacus luctus accumsan tortor posuere ac ut consequat semper viverra. 
-          Tellus cras adipiscing enim eu turpis.
-          </p>
+          <p className=" flex text-sm font-light sm:text-sm md:text-lg lg:text-xl xl:text-2xl">
+          At Vibey we aim to provide timely and comprehensive details across various 
+          industries, fostering a community of collaboration and knowledge sharing. 
+          By offering enriching opportunities, we empower individuals and organizations to
+          stay ahead in their fields, promoting growth, innovation, and meaningful 
+          connections within the global tech community.</p>
         </div>
-        <div className="pt-24 pl-6 mt-10  basis-1/2  text-center mx-auto">
+        <div className="pt-24 pl-6 mt-10  basis-1/2 md:mt-0 text-center mx-auto">
             <Image width={400} height={300} src= "/static/vibeydesign.png" alt="logo" className="dark:hidden text-center mx-auto "/> 
             <Image width={400} height={300} src="/static/vibeydesign.png" alt="logo" className="hidden dark:block text-center mx-auto"/>     
-        </div>   
+        </div> 
       </div>
       <div className="font-bold my-16 text-center md:text-2xl pt-14">
           <span className="pt-40 text-transparent bg-gradient-to-bl from-[rgb(178,15,255)] to-[#ff5100] bg-clip-text text-2xl">
             Our Team
           </span>
       </div>
-      <div className="flex justify-center gap-14 items-center pb-14">
-        <div className="card justify-center w-fit h-fit my-15">
+      <div className=" justify-center gap-10 pb-14 mx-20  grid grid-cols-1 sm:text-left sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4">
+        <div className="card justify-center w-fit h-fit my-15 mx-auto">
           <Image width={128} height={128} src= "/images/events/DeepandraKumar.png" alt="logo" className="mx mt-3 mb-6-8 h-32 rounded-full  dark:hidden text-center  overflow-hidden"/>
           <Image width={128} height={128} src="/images/events/DeepandraKumar.png" alt="logo" className="mx-8 mt-3 mb-6 h-32  rounded-full  dark:block text-center overflow-hidden"/>     
           <h6 className="font-bold text-center">Deepandra Kumar</h6>
@@ -120,9 +120,9 @@ const about = () => {
           />
           </div>
         </div>          
-        <div className="card justify-center w-fit h-fit my-15">
-          <Image width={128} height={128} src= "/images/events/Kritika.png" alt="logo" className="mx mt-3 mb-6-8 h-32 rounded-full  dark:hidden text-center  overflow-hidden"/>
-          <Image width={128} height={128} src="/images/events/Kritika.png" alt="logo" className="mx-8 mt-3 mb-6 h-32  rounded-full  dark:block text-center overflow-hidden"/>     
+        <div className="card justify-center w-fit h-fit my-15 mx-auto">
+          <Image width={128} height={128} src= "/images/events/kritika.png" alt="logo" className="mx mt-3 mb-6-8 h-32 rounded-full  dark:hidden text-center  overflow-hidden"/>
+          <Image width={128} height={128} src="/images/events/kritika.png" alt="logo" className="mx-8 mt-3 mb-6 h-32  rounded-full  dark:block text-center overflow-hidden"/>     
           <h6 className="font-bold text-center">Kritika Goyal</h6>
           <h6 className="font-light text-center">Designer, Tech Lead</h6>
           <div className="flex justify-center gap-4">
@@ -144,7 +144,7 @@ const about = () => {
           />
           </div>
         </div>
-        <div className="card justify-center w-fit h-fit my-15">
+        <div className="card justify-center w-fit h-fit my-15 mx-auto">
           <Image width={128} height={128} src= "/images/events/ManishTyagi.png" alt="logo" className="mx mt-3 mb-6-8 h-32 rounded-full  dark:hidden text-center  overflow-hidden"/>
           <Image width={128} height={128} src="/images/events/ManishTyagi.png" alt="logo" className="mx-8 mt-3 mb-6 h-32  rounded-full  dark:block text-center overflow-hidden"/>     
           <h6 className="font-bold text-center">Manish Tyagi</h6>
@@ -168,9 +168,9 @@ const about = () => {
           />
           </div>
         </div> 
-        <div className="card justify-center w-fit h-fit my-15">
-          <Image width={128} height={128} src= "/images/events/Chandraprakash.png" alt="logo" className="mx mt-3 mb-6-8 h-32 rounded-full  dark:hidden text-center  overflow-hidden"/>
-          <Image width={128} height={128} src="/images/events/Chandraprakash.png" alt="logo" className="mx-8 mt-3 mb-6 h-32  rounded-full  dark:block text-center overflow-hidden"/>     
+        <div className="card justify-center w-fit h-fit mx-auto my-15">
+          <Image width={128} height={128} src= "/images/events/chandraprakash.png" alt="logo" className="mx mt-3 mb-6-8 h-32 rounded-full  dark:hidden text-center  overflow-hidden"/>
+          <Image width={128} height={128} src="/images/events/chandraprakash.png" alt="logo" className="mx-8 mt-3 mb-6 h-32  rounded-full  dark:block text-center overflow-hidden"/>     
           <h6 className="font-bold text-center">Chandraprakash</h6>
           <h6 className="font-light text-center">Designer, Tech Lead</h6>
           <div className="flex justify-center gap-4">
@@ -198,4 +198,5 @@ const about = () => {
 };
 
 export default about;
+
 
