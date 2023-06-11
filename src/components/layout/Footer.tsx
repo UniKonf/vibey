245dc6aec772fa97da4f78425d99718a4d20a545
Vishal K <116670999+Vishalk91-4@users.noreply.github.com>
2023-06-11 23:09:04 +0530

    Enhanced Footer.tsx (#233)
    * Enhanced Footer.tsx
    
    * Update Footer.tsx
    
    * Add files via upload
    
    * Add files via upload
    
    * Delete About.tsx
    
    * Updated Footer.tsx
    
    * Updated Footer.tsx
    
    * Updated Footer.tsx
    
    * Aligned Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,14 +1,10 @@
-import { Logo } from '@/components';
-import IconLink from '@/components/links/IconLink';
-import UnstyledLink from '@/components/links/UnstyledLink';
 
 import VercelLogo from '@/Icons/VercelLogo';
 
+import Image from "next/image";
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { FC } from 'react';
-import { BsDiscord } from 'react-icons/bs';
-import { RiGithubFill, RiMailLine, RiTwitterFill } from 'react-icons/ri';
 
 const Footer: FC = () => {
   const router = useRouter();
@@ -17,101 +13,92 @@ const Footer: FC = () => {
   return (
     <section className="py-10">
       <div className="layout mx-auto">
-        <hr className="border-foreground/30 pb-5" />
+        <hr className="border-base-content/30 pb-5" />
 
-        <div className="flex flex-col items-center justify-between gap-4">
-          {/* logo */}
-          <Logo />
+        <div className="justify-content mx-auto w-11/12 gap-14 md:flex">
+         <div className="mt-16 basis-1/2 md:mt-0">
+            <Link href="/_document">
+               <Image width={200} height={200} src= "/static/Vibey-banner.png" alt="logo" className=" bg-transparent dark:hidden"/> 
+               <Image width={200} height={200} src="/static/Vibey-banner.png" alt="logo" className="bg-transparent hidden dark:block"/>
+            </Link>
+            <p className=" mt-5">
+            The One Spot for all tech Conference, Workshops and Events</p>
+            <p className="text-primary">Find Your Next Developer Event
+            </p>
+         </div>
+         <div className="mt-16 basis-1/3 md:mt-0">
+            <h4 className="font-bold text-center text-primary">Latest</h4>
+            <hr style={{ borderColor: '#ff5100'}}></hr>
+            <ul className="list-none m-0 p-0 font-medium text-center justify-center">
+               <li className="my-6">
+                  <Link href="/https://jsconf.in/" className=" hover:underline">
+                     Latest CFP
+                  </Link>
+               </li>
+               <li  className="my-6">
+                  <Link href="/" className="hover:underline ">
+                  Hackathon
+                  </Link>
+               </li>
+               <li  className="my-6">
+                  <Link href="/" className=" hover:underline ">
+                  Conference
+                  </Link>
+               </li>
+            </ul>
+         </div>
+         <div className="mt-16 basis-1/3 md:mt-0">
+            <h4 className="font-bold text-center text-primary">Quick Links</h4>
+            <hr style={{ borderColor: '#ff5100'}}></hr>
+            <ul className="list-none m-0 p-0 font-medium text-center justify-center">
+               <li className="my-6">
+                  <Link href="/About" className="hover:underline">
+                     About Us
+                  </Link>
+               </li>
+               <li className="my-6">
+                  <Link href="/contact" className="  hover:underline">
+                     Contact Us
+                  </Link>
+               </li>
+               <li className="my-6">
+                  <Link href="/privacy" className="hover:underline text-center">
+                     Privacy Policy
+                  </Link>
+               </li>
 
-          {/* social icons */}
-          <ul className="flex items-center space-x-3">
-            <li>
-              <IconLink
-                variant="outline"
-                aria-label="Visit us on Twitter"
-                title="Twitter (External Link)"
-                href="https://twitter.com/vibeydotlive"
-                icon={RiTwitterFill}
-              />
-            </li>
-            <li>
-              <IconLink
-                variant="outline"
-                aria-label="Join us on Discord"
-                title="Discord (External Link)"
-                href="https://discord.gg/erHegt9UTf"
-                icon={BsDiscord}
-              />
-            </li>
-            <li>
-              <IconLink
-                variant="outline"
-                aria-label="Mail us on"
-                title="Mail (External Link)"
-                href="mailto:unikonf.org@gmail.com"
-                icon={RiMailLine}
-              />
-            </li>
+            </ul>
+         </div>
+         <div className="mt-16 basis-1/3 md:mt-0">
+         <h4 className="font-bold text-center text-primary">Socials</h4>
+         <hr style={{ borderColor: '#ff5100'}}></hr>
+            <ul className="list-none m-0 p-0 font-medium text-center justify-center">
+               <li className="my-6">
 
-            <li>
-              <IconLink
-                variant="outline"
-                aria-label="Visit us on Github"
-                title="Github (External Link)"
-                href="https://github.com/UniKonf/vibey"
-                icon={RiGithubFill}
-              />
-            </li>
-          </ul>
+                  <Link href="https://github.com/UniKonf/vibey" className="text-center hover:underline p-3">GitHub</Link>
+               </li>
+               <li className="my-6">
+
+                  <Link href="https://twitter.com/vibeydotlive" className=" hover:underline px-3 pb-4">Twitter</Link>                 
+               </li>
+               <li className="my-6">
+
+                  <Link href="https://discord.gg/erHegt9UTf" className=" hover:underline px-3 pt-3 pb-4">Discord</Link>
+               </li>
+            </ul>
+         </div>
+         
         </div>
+      <div className="mt-12 mx-auto w-11/12 gap-14 flex">
+         <div className=" basis-1/2 ">
+            Vibey © {new Date().getFullYear()}. All Rights Reserved.
+         </div>
+         <div className=" basis-1/2 text-end">
+            <Link href="/terms" className="hover:underline font-light text-sm ">Terms & Condition</Link>
+         </div>
       </div>
-
-      <div className="mt-4 text-center">
-        {/* thanks contributors */}
-        <p className="pb-4">
-          Thanks to all the awesome contributors to maintain{' '}
-          <UnstyledLink
-            href="https://github.com/UniKonf/vibey"
-            className="text-color-pink hover:underline"
-          >
-            Vibey
-          </UnstyledLink>
-          . Don't forget to leave a star 🌟
-        </p>
-
-        {/* inportant links */}
-        <p className="">
-          <Link
-            href="/contact"
-            className="px-2 text-color-pink hover:underline"
-          >
-            Contact
-          </Link>{' '}
-          |{' '}
-          <Link href="/about" className="px-2 text-color-pink hover:underline">
-            About
-          </Link>{' '}
-          |{' '}
-          <Link href="/terms" className="px-2 text-color-pink hover:underline">
-            Terms &amp; Conditions
-          </Link>{' '}
-          |{' '}
-          <Link
-            href="/privacy"
-            className="px-2 text-color-pink hover:underline"
-          >
-            Privacy Policy
-          </Link>
-        </p>
-
-        {/* copyrights */}
-        <p className="mt-8 w-full py-5 text-center text-sm md:order-2 md:mt-0 md:w-auto">
-          © {new Date().getFullYear()} Vibey. All Rights Reserved.
-        </p>
       </div>
-
-      {/* vercel */}
-      <div className="mt-4 flex justify-center">
+      <div className="mt-4 flex justify-center pl-">
         <VercelLogo />
       </div>
     </section>
@@ -119,3 +106,4 @@ const Footer: FC = () => {
 };
 
 export default Footer;
+
