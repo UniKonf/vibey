de255a02d5defdc7d44a6d728999ed0d07488558
Tanishqa Mitra <tanishqamitra79@gmail.com>
2023-07-02 19:05:36 +0530

    fixed

--- src/components/Search/SearchHits.tsx
+++ src/components/Search/SearchHits.tsx
@@ -3,7 +3,8 @@ import { EventInterface } from '../../lib/types';
 import Event from '../Event';
 import Heading from '../Heading';
 import { FC, useContext, useEffect, useRef } from 'react';
-import { connectHits, HitsProvided } from 'react-instantsearch-core';
+import { connectHits } from 'react-instantsearch-core';
+import { HitsProvided } from 'react-instantsearch-core';
 import { SearchBox } from 'react-instantsearch-dom';
 
 type Props = HitsProvided<EventInterface>;
