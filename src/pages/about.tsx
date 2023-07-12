fbcb0c7f5660ec543f3906c8223f4edfc3865706
Tanishqa Mitra <tanishqamitra79@gmail.com>
2023-07-12 16:59:22 +0530

    commit local changes

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -8,6 +8,42 @@ import Link from 'next/link';
 import React from 'react';
 import { RiGithubFill, RiMailLine } from 'react-icons/ri';
 
+const profiles = [
+  {
+    name: 'Deependra Kumar',
+    email: 'deependrarajpoot178@gmail.com',
+    github: 'https://github.com/Deepu178',
+    title: 'Tech Lead & Maintainer',
+    image: '/images/events/deependra.png',
+  },
+  {
+    name: 'Kritika Goyal',
+    email: 'https://github.com/Kirtikagoyal',
+    github: 'https://github.com/Kirtikagoyal',
+    title: 'Project Coordinator',
+    image: '/images/events/Kritika.png',
+  },
+  {
+    name: 'Manish Tyagi',
+    email: ' manishtyagi088@gmail.com',
+    github: 'https://github.com/money8203',
+    title: 'Project Manager & Maintainer',
+    image: '/images/events/ManishTyagi.png',
+  },
+  {
+    name: 'Chandraprakash Darji',
+    email: 'prakashchandra3786@gmail.com',
+    github: 'https://github.com/Chandraprakash-Darji',
+    title: 'Maintainer',
+    image: '/images/events/Chandraprakash.png',
+  },
+];
+
+const events = [
+  { name: 'Events', image: '/static/event.jpg', link: '/events' },
+  { name: 'Confrences', image: '/static/hackathon.jpg', link: '/confrences' },
+  { name: 'CFPs', image: '/static/cfp.jpg', link: '/cfps' },
+];
 const about = () => {
   return (
     <>
@@ -51,77 +87,34 @@ const about = () => {
           All that's happening around in the Tech World
         </h3>
       </div>
-      <div className="mx-20 justify-center items-center overflow-hidden grid grid-cols-1 gap-3 sm:text-left sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
-        <div className="card justify-center text-center px-0 pt-0 mx-auto w-min">
-          <Image
-            width={200}
-            height={200}
-            src="/static/event.jpg"
-            alt="logo"
-            className=" rounded-lg w-full h-56 dark:hidden text-center mx-auto overflow-hidden "
-          />
-          <Image
-            width={200}
-            height={200}
-            src="/static/event.jpg"
-            alt="logo"
-            className="rounded-lg w-full h-56 hidden dark:block text-center overflow-hidden"
-          />
-          <h2 className="mt-10">Events</h2>
-          <span className="underline whitespace-pre w-full block py-3"> </span>
-          <Link href="/events">
-            <Button type="button" className={clsxm(`items-center py-3`)}>
-              Know More
-            </Button>
-          </Link>
-        </div>
-        <div className="card justify-center text-center px-0 pt-0 mx-auto w-min">
-          <Image
-            width={200}
-            height={200}
-            src="/static/hackathon.jpg"
-            alt="logo"
-            className="rounded-lg w-full h-56 dark:hidden text-center mx-auto overflow-hidden"
-          />
-          <Image
-            width={200}
-            height={200}
-            src="/static/hackathon.jpg"
-            alt="logo"
-            className="rounded-lg w-full h-56 hidden dark:block text-center overflow-hidden"
-          />
-          <h2 className="mt-10">Conference</h2>
-          <span className="underline whitespace-pre w-full block py-3 "> </span>
-          <Link href="/events">
-            <Button type="button" className={clsxm(`items-center py-3`)}>
-              Know More
-            </Button>
-          </Link>
-        </div>
-        <div className="card justify-center text-center px-0 pt-0 mx-auto w-min">
-          <Image
-            width={200}
-            height={200}
-            src="/static/cfp.jpg"
-            alt="logo"
-            className="rounded-lg h-56 w-full dark:hidden text-center mx-auto overflow-hidden"
-          />
-          <Image
-            width={200}
-            height={200}
-            src="/static/cfp.jpg"
-            alt="logo"
-            className="rounded-lg h-56 w-full hidden dark:block text-center overflow-hidden"
-          />
-          <h2 className="mt-10">CFPs</h2>
-          <span className="underline whitespace-pre w-full block py-3"> </span>
-          <Link href="/events">
-            <Button type="button" className={clsxm(`items-center py-3`)}>
-              Know More
-            </Button>
-          </Link>
-        </div>
+      <div className="flex flex-col gap-10  lg:flex-row ">
+        {events.map((event) => (
+          <div
+            key={event.name}
+            className="mx-20 justify-center items-center overflow-hidden h-84 basis-1/3"
+          >
+            <div className="card justify-center text-center h-96">
+              <Image
+                width={200}
+                height={200}
+                src={event.image}
+                alt="logo"
+                className=" rounded-lg w-full h-40 text-center mx-auto  "
+              />
+              <h2 className="mt-10">{event.name}</h2>
+              <span className="underline whitespace-pre w-full block py-3">
+                {' '}
+              </span>
+              <Link href={event.link}>
+                <Button type="button" className={clsxm(`items-center py-3`)}>
+                  Know More
+                </Button>
+              </Link>
+            </div>
+          </div>
+        ))}
       </div>
+
       <div className="justify-content pl-6 mx-auto w-11/12 gap-10 md:flex ">
         <div className="text-center pt-40 mt-5 basis-1/2  font-bold sm:text-lg md:text-xl lg:text-2xl xl:text-4xl">
           <span className="pt-40 text-transparent bg-gradient-to-bl from-[rgb(178,15,255)] to-[#ff5100] bg-clip-text">
@@ -160,152 +153,47 @@ const about = () => {
         </span>
       </div>
       <div className=" justify-center gap-10 pb-14 mx-20  grid grid-cols-1 sm:text-left sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4">
-        <div className="card justify-center w-fit h-fit my-15 mx-auto">
-          <Image
-            width={128}
-            height={128}
-            src="/images/events/DeepandraKumar.png"
-            alt="logo"
-            className="mx mt-3 mb-6-8 h-32 rounded-full  dark:hidden text-center  overflow-hidden"
-          />
-          <Image
-            width={128}
-            height={128}
-            src="/images/events/DeepandraKumar.png"
-            alt="logo"
-            className="mx-8 mt-3 mb-6 h-32  rounded-full  dark:block text-center overflow-hidden"
-          />
-          <h6 className="font-bold text-center">Deepandra Kumar</h6>
-          <h6 className="font-light text-center">Tech Lead & Maintainer</h6>
-          <div className="flex justify-center gap-4">
-            <IconLink
-              variant="outline"
-              aria-label="Visit us on Github"
-              title="Github (External Link)"
-              href="https://github.com/Deepu178"
-              icon={RiGithubFill}
-              className="items-center w-fit flex my-3"
+        {profiles.map((profile) => (
+          <div
+            key={profile.name}
+            className="card justify-center w-fit h-fit my-15 mx-auto"
+          >
+            <Image
+              width={128}
+              height={128}
+              src={profile.image}
+              alt="logo"
+              className="mx mt-3 mb-6-8 h-32 rounded-full  dark:hidden text-center  overflow-hidden"
             />
-            <IconLink
-              variant="outline"
-              aria-label="Mail us on"
-              title="Mail (External Link)"
-              href="mailto: deependrarajpoot178@gmail.com"
-              icon={RiMailLine}
-              className="items-center w-fit flex my-3"
+            <Image
+              width={128}
+              height={128}
+              src={profile.image}
+              alt="logo"
+              className="mx-8 mt-3 mb-6 h-32  rounded-full  dark:block text-center overflow-hidden"
             />
+            <h6 className="font-bold text-center">{profile.name}</h6>
+            <h6 className="font-light text-center">{profile.title}</h6>
+            <div className="flex justify-center gap-4">
+              <IconLink
+                variant="outline"
+                aria-label="Visit us on Github"
+                title="Github (External Link)"
+                href={profile.github}
+                icon={RiGithubFill}
+                className="items-center w-fit flex my-3"
+              />
+              <IconLink
+                variant="outline"
+                aria-label="Mail us on"
+                title="Mail (External Link)"
+                href={`mailto: ${profile.email}`}
+                icon={RiMailLine}
+                className="items-center w-fit flex my-3"
+              />
+            </div>
           </div>
-        </div>
-        <div className="card justify-center w-fit h-fit my-15 mx-auto">
-          <Image
-            width={128}
-            height={128}
-            src="/images/events/Kritika.png"
-            alt="logo"
-            className="mx mt-3 mb-6-8 h-32 rounded-full  dark:hidden text-center  overflow-hidden"
-          />
-          <Image
-            width={128}
-            height={128}
-            src="/images/events/Kritika.png"
-            alt="logo"
-            className="mx-8 mt-3 mb-6 h-32  rounded-full  dark:block text-center overflow-hidden"
-          />
-          <h6 className="font-bold text-center">Kritika Goyal</h6>
-          <h6 className="font-light text-center">Project Coordinator</h6>
-          <div className="flex justify-center gap-4">
-            <IconLink
-              variant="outline"
-              aria-label="Visit us on Github"
-              title="Github (External Link)"
-              href="https://github.com/Kirtikagoyal"
-              icon={RiGithubFill}
-              className="items-center w-fit flex my-3"
-            />
-            <IconLink
-              variant="outline"
-              aria-label="Mail us on"
-              title="Mail (External Link)"
-              href="mailto: goyalkirtika.077@gmail.com"
-              icon={RiMailLine}
-              className="items-center w-fit flex my-3"
-            />
-          </div>
-        </div>
-        <div className="card justify-center w-fit h-fit my-15 mx-auto">
-          <Image
-            width={128}
-            height={128}
-            src="/images/events/ManishTyagi.png"
-            alt="logo"
-            className="mx mt-3 mb-6-8 h-32 rounded-full  dark:hidden text-center  overflow-hidden"
-          />
-          <Image
-            width={128}
-            height={128}
-            src="/images/events/ManishTyagi.png"
-            alt="logo"
-            className="mx-8 mt-3 mb-6 h-32  rounded-full  dark:block text-center overflow-hidden"
-          />
-          <h6 className="font-bold text-center">Manish Tyagi</h6>
-          <h6 className="font-light text-center">
-            Project Manager & Maintainer
-          </h6>
-          <div className="flex justify-center gap-4">
-            <IconLink
-              variant="outline"
-              aria-label="Visit us on Github"
-              title="Github (External Link)"
-              href="https://github.com/money8203"
-              icon={RiGithubFill}
-              className="items-center w-fit flex my-3"
-            />
-            <IconLink
-              variant="outline"
-              aria-label="Mail us on"
-              title="Mail (External Link)"
-              href="mailto: manishtyagi088@gmail.com"
-              icon={RiMailLine}
-              className="items-center w-fit flex my-3"
-            />
-          </div>
-        </div>
-        <div className="card justify-center w-fit h-fit mx-auto my-15">
-          <Image
-            width={128}
-            height={128}
-            src="/images/events/Chandraprakash.png"
-            alt="logo"
-            className="mx mt-3 mb-6-8 h-32 rounded-full  dark:hidden text-center  overflow-hidden"
-          />
-          <Image
-            width={128}
-            height={128}
-            src="/images/events/Chandraprakash.png"
-            alt="logo"
-            className="mx-8 mt-3 mb-6 h-32  rounded-full  dark:block text-center overflow-hidden"
-          />
-          <h6 className="font-bold text-center">Chandraprakash Darji</h6>
-          <h6 className="font-light text-center">Maintainer</h6>
-          <div className="flex justify-center gap-4">
-            <IconLink
-              variant="outline"
-              aria-label="Visit us on Github"
-              title="Github (External Link)"
-              href="https://github.com/Chandraprakash-Darji"
-              icon={RiGithubFill}
-              className="items-center w-fit flex my-3"
-            />
-            <IconLink
-              variant="outline"
-              aria-label="Mail us on"
-              title="Mail (External Link)"
-              href="mailto: prakashchandra3786@gmail.com"
-              icon={RiMailLine}
-              className="items-center w-fit flex my-3"
-            />
-          </div>
-        </div>
+        ))}
       </div>
     </>
   );
