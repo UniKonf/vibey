d05262c7db2943dbe0f32e0e3c44e17e132112e7
Varghab <shibvarghab@gmail.com>
2023-07-17 11:20:52 +0530

    Server set up locally

--- server/schema/mongo.connect.js
+++ server/schema/mongo.connect.js
@@ -4,18 +4,17 @@ import mongoose, { connect } from 'mongoose';
 mongoose.set('strictQuery', false);
 
 export const mongoConnect = async () => {
-  const connectionString =
-    process.env.MONGODB_PROTO +
-    process.env.MONGODB_USER +
-    ':' +
-    process.env.MONGODB_PASSWORD +
-    '@' +
-    process.env.MONGODB_URL +
-    '/' +
-    process.env.MONGODB_DATABASE;
+  const connectionString = 'mongodb://127.0.0.1:27017/vibey';
+  // process.env.MONGODB_PROTO +
+  // process.env.MONGODB_USER +
+  // ':' +
+  // process.env.MONGODB_PASSWORD +
+  // '@' +
+  // process.env.MONGODB_URL +
+  // '/' +
+  // process.env.MONGODB_DATABASE;
   try {
     // eslint-disable-next-line no-console
-    console.log('connection');
     await connect(connectionString);
   } catch (error) {
     throw new Error(error);
