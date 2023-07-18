916904a2a262c7034935f840e3ada8aa08daf1cf
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-07-19 00:55:10 +0530

    refactor: add interface and missing properties
    use Link component in place of anchor tag for better optimization

--- src/pages/Contributors/GetContributor.tsx
+++ src/pages/Contributors/GetContributor.tsx
@@ -1,8 +1,15 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { useEffect, useState } from 'react';
+interface Contributor {
+  login: string;
+  avatar_url: string;
+  contributions: number;
+  html_url: string;
+}
 
 function GetContributor() {
-  const [contributors, setContributors] = useState([]);
+  const [contributors, setContributors] = useState<Contributor[]>([]);
 
   const getData = async () => {
     const res = await fetch(
@@ -11,7 +18,7 @@ function GetContributor() {
 
     const data = await res.json();
     const contributorsData = data.filter(
-      (contributor: any) => !contributor.login.includes('dependabot[bot]')
+      (contributor: Contributor) => !contributor.login.includes('dependabot[bot]')
     );
     setContributors(contributorsData);
   };
@@ -29,7 +36,7 @@ function GetContributor() {
         className="w-[100%] flex flex-wrap justify-evenly pt-10"
         style={{ rowGap: '1.5rem' }}
       >
-        {contributors?.map((contributor: any, i) => (
+        {contributors?.map((contributor: Contributor, i) => (
           <div
             className="w-[80%] md:w-[26%] border-2 border-black py-4 px-5 rounded-xl flex items-center flex-col dark:border-white space-y-3 
             hover:scale-105 ease-linear shadow-xl shadow-slate-700"
@@ -39,14 +46,16 @@ function GetContributor() {
               className="rounded-full w-52 hover:scale-105"
               src={contributor.avatar_url}
               alt={contributor.login}
+              height={204}
+              width={204}
             />
             <p className="text-xl">{contributor.login}</p>
             <p>{contributor.contributions} Commits</p>
-            <a href={contributor.html_url} target="_blank" rel="noreferrer">
+            <Link href={contributor.html_url} target="_blank" rel="noreferrer">
               <button className="px-20 py-2 rounded-lg bg-violet-500">
                 Profile
               </button>
-            </a>
+            </Link>
           </div>
         ))}
       </div>
