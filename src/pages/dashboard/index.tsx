a7205afac28b030d0ffc4a5935f1df6fd4ef12b0
SouravvMishra <souravvmishra@gmail.com>
2023-08-01 20:15:51 +0530

    Toast Now Appear Only After The Login Has Been Successfull

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -3,14 +3,31 @@ import { SettingsContext } from '@/lib/context/settings';
 import DashboadEventPage from '@/components/Dashboard/Events';
 
 import Image from 'next/image';
+import { useSession } from 'next-auth/react';
 import { useContext } from 'react';
 import { useState } from 'react';
-import React from 'react';
-import { ToastContainer } from 'react-toastify';
+import React, { useEffect } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
 
 const Dashboard = () => {
   const { theme } = useContext(SettingsContext);
   const [selectedOption, setSelectedOption] = useState<string>('');
+  const { data: session, status } = useSession();
+
+  useEffect(() => {
+    if (session && status) {
+      toast.success(`Welcome ${session.user?.name?.toUpperCase()}!`, {
+        position: 'top-right',
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: theme,
+      });
+    }
+  }, [session]);
 
   return (
     <div className="flex flex-1">
@@ -25,6 +42,8 @@ const Dashboard = () => {
         draggable
         pauseOnHover
         closeButton={false}
+        limit={1}
+        // toastClassName={'h-8 w-10'}
       />
       <div className="mt-16 hidden border-r-2 border-white/10 md:flex md:w-64 md:flex-col">
         <div className="flex flex-grow flex-col overflow-y-auto pt-5">
