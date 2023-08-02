a395bc71cd5a6036fa45f30c33743eb222108a28
Suzy <sulagnaghosh52@gmail.com>
2023-08-02 11:50:25 +0530

    feat: add delete event feature

--- server/schema/mongo.connect.js
+++ server/schema/mongo.connect.js
@@ -5,6 +5,15 @@ mongoose.set('strictQuery', false);
 
 export const mongoConnect = async () => {
   const connectionString = process.env.DB;
+  // const connectionString =
+  //   process.env.MONGODB_PROTO +
+  //   process.env.MONGODB_USER +
+  //   ':' +
+  //   process.env.MONGODB_PASSWORD +
+  //   '@' +
+  //   process.env.MONGODB_URL +
+  //   '/' +
+  //   process.env.MONGODB_DATABASE;
   try {
     // eslint-disable-next-line no-console
     await connect(connectionString);
--- src/components/Dashboard/Events.tsx
+++ src/components/Dashboard/Events.tsx
@@ -12,6 +12,7 @@ import { toast, ToastContainer } from 'react-toastify';
 const DashboadEventPage = () => {
   const [allEventsData, setAllEventsData] = useState<DashboardEventType[]>([]);
   const [initialLoading, setInitialLoading] = useState<boolean>(true);
+  const [deleteId, setDeleteId] = useState<string>('');
 
   const getData = async () => {
     setInitialLoading(true);
@@ -19,7 +20,7 @@ const DashboadEventPage = () => {
       const res = await fetch(`/api/events/allevents`).then((response) =>
         response.json()
       );
-      // console.log(res)
+
       if (res.success) {
         setAllEventsData(res.events);
         setInitialLoading(false);
@@ -41,6 +42,12 @@ const DashboadEventPage = () => {
   useEffect(() => {
     getData();
   }, []);
+
+  useEffect(() => {
+    setAllEventsData((prevData) =>
+      prevData.filter((data: DashboardEventType) => data._id !== deleteId)
+    );
+  }, [deleteId]);
 
   return (
     <div className="relative z-10 rounded-3xl ">
@@ -71,7 +78,11 @@ const DashboadEventPage = () => {
               <div className="events grid grid-cols-auto-sm gap-7">
                 {allEventsData.map(
                   (event: DashboardEventType, index: number) => (
-                    <DashboardEventCard {...event} key={index} />
+                    <DashboardEventCard
+                      event={event}
+                      setDeleteId={setDeleteId}
+                      key={index}
+                    />
                   )
                 )}
               </div>
